9ab1d10a5c443339bf65da00f0b768f45114fb95
Ville Valtonen <51400922+Foksu@users.noreply.github.com>
2020-02-28 20:47:09 +0200

    Add files via upload

---
+++ rstr_capitalizer.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+
+#define TOLOWER(c) (c | ' ')
+#define TOUPPER(c) (c & '_')
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int		ft_isspace(char str)
+{
+	if (str == ' ' || str == '\t')
+		return (1);
+	return (0);
+}
+
+void 	rstr_capitalizer(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		while (str[i] && ft_isspace(str[i]))
+			ft_putchar(str[i++]);
+		if (!(ft_isspace(str[i + 1])))
+			ft_putchar(TOLOWER(str[i++]));
+		while (str[i] && ((ft_isspace(str[i + 1]) || (str[i + 1] == '\0'))))
+			ft_putchar(TOUPPER(str[i++]));
+	}
+}
+
+int 	main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	if (argc > 1)
+	{
+		*argv++;
+		while (*argv)
+		{
+			rstr_capitalizer(*argv++);
+			write(1, "\n", 1);
+		}
+	}
+	return (0);
+}
