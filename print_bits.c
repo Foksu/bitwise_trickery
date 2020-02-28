2974b78213cc38e50768434e10c2c3bebc9332de
Ville Valtonen <51400922+Foksu@users.noreply.github.com>
2020-02-28 20:42:18 +0200

    Add files via upload
    Added main for testing purposes

--- print_bits.c
+++ print_bits.c
@@ -7,3 +7,25 @@ void	print_bits(unsigned char octet)
 	while (i >>= 1)
 		(octet & i) ? write(1, "1", 1) : write(1, "0", 1);
 }
+
+void	print_bits2(unsigned char octet)
+{
+	int	i;
+
+	i = 128;
+	while (octet >= 0 && i)
+	{
+		(octet / i) ? write(1, "1", 1) : write(1, "0", 1);
+		(octet / i) ? octet -= i : 0;
+		i /= 2;
+	}
+}
+
+int		main(void)
+{
+	int n = 64;
+	print_bits(n);
+	write(1, "\n", 1);
+	print_bits2(n);
+    write(1, "\n", 1);
+}
