2523c3ea29e6445f156bbf6dbcc0b15384d656eb
Ville Valtonen <51400922+Foksu@users.noreply.github.com>
2020-02-28 21:04:28 +0200

    Add files via upload
    Added additional functions

--- reverse_bits.c
+++ reverse_bits.c
@@ -21,12 +21,30 @@ unsigned char reverse_bits(unsigned char b)
 	return (r);
 }
 
+unsigned char reverse_bits2(unsigned char b)
+{
+	b = (b & 0xF0) >> 4 | (b & 0x0F) << 4; // 1)
+	b = (b & 0xCC) >> 2 | (b & 0x33) << 2; // 2)
+	b = (b & 0xAA) >> 1 | (b & 0x55) << 1; // 3)
+	return (b);
+}
+
+unsigned char reverse_bits3(unsigned char b)
+{
+	b = (b * 0x0202020202ULL & 0x010884422010ULL) % 0x3ff;
+	return (b);
+}
+
 int main(void)
 {
 	int n = 64;
 	print_bits(n);
 	write(1, "\n", 1);
 	print_bits(reverse_bits(n));
+	write(1, "\n", 1);
+	print_bits(reverse_bits2(n));
+	write(1, "\n", 1);
+	print_bits(reverse_bits3(n));
 	write(1, "\n", 1);
 	return (0);
 }
